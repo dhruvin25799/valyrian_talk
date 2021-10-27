9a64e76e9fb0c7fb8f4501dc9f8bf5b40a43f8fa
Dhruvin Mehta <dhruvin.mehta25799@gmail.com>
2021-10-27 14:47:42 +0530

    Added files

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+// welcome to event based programming
+// and callback
+
+
+// button click
+var translateButton = document.querySelector("#translate-button");
+
+translateButton.addEventListener("click", buttonClickHandler)
+
+// read input
+var translateInput = document.querySelector("#translate-input");
+
+var translateOutput = document.querySelector("#translate-output");
+
+
+var url = "https://api.funtranslations.com/translate/valyrian.json"
+
+
+function buttonClickHandler(event) {
+    console.log("Button pressed");
+    var input = translateInput.value;
+    var finalURL = constructURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            translateOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("Error"))
+
+}
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
